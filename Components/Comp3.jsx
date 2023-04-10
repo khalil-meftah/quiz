0abff73b4036c8f403be1c28c2a9a76a84bb5d12
KHOULOUD SAHMOUNE <khouloud.sahmoune@gmail.com>
2023-04-10 13:59:35 +0000

    deleted files

--- Components/Comp3.jsx
+++
@@ -1,8 +0,0 @@
-import React, {Component} from "react";
-
-export default class Comp3 extends Component{
-    render(){
-        const {nom, prenom} = this.props;
-        return(<h1>hello {nom} {prenom}</h1>);
-    }
-}
