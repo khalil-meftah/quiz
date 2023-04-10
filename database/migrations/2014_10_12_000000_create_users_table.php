a98a4dc1428931b93a4d68058998f52e34c602e7
khalil-meftah <khalile.meftah@gmail.com>
2023-04-10 15:53:31 +0000

    added migration tables and models

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,11 +13,14 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('nom');
+            $table->string('prenom');
+            $table->string('role');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->rememberToken();
+            $table->date('dateDeNaissance');
+            $table->string('adresse');
+            $table->string('numeroDeTelephone');
             $table->timestamps();
         });
     }
---
+++ database/migrations/2023_04_10_154957_create_chapitres_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('chapitres', function (Blueprint $table) {
+            $table->id();
+            $table->text('descriptionChapitre')->nullable();
+            $table->integer('nombreHeuresChapitre');
+            $table->date('dateDebutChapitre');
+            $table->date('dateCreationChapitre');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('chapitres');
+    }
+};
