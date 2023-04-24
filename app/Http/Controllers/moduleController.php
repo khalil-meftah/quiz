91bda02e93ce41c00191d7a1e37406db51b57ea3
khalil-meftah <khalile.meftah@gmail.com>
2023-04-24 13:12:04 +0100

    adding moduleController

---
+++ app/Http/Controllers/moduleController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\module;
+class moduleController extends Controller
+{
+    public function index()
+    {
+        $mod = module::all();
+        //module is the view module.blade
+        //compact is the auto table that contains the variables of modules
+        return view('module' , compact('mod'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
